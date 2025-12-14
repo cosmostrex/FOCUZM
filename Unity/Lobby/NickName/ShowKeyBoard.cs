8728921e0e3baa86c0801f8435e17ded21856763
Yu Chaeeun <imcosmostrex@gmail.com>
2025-12-14 19:20:35 +0900

    10.02.

--- Unity/Lobby/NickName/ShowKeyBoard.cs
+++ Unity/Lobby/NickName/ShowKeyBoard.cs
@@ -1,1 +1,26 @@
+// 2025.10.02
+// ShowKeyBoard
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Microsoft.MixedReality.Toolkit.Experimental.UI;
+
+public class ShowKeyBoard : MonoBehaviour
+{
+    private TMP_InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onSelect.AddListener(x => OpenKeyboard());
+    }
+
+    // Update is called once per frame
+    public void OpenKeyboard()
+    {
+        NonNativeKeyboard.Instance.InputField = inputField;
+        NonNativeKeyboard.Instance.PresentKeyboard(inputField.text);
+    }
+}
